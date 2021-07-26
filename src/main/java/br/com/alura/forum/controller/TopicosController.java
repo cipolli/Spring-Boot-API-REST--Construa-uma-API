587b63570bd4924d6eee472daff6941737405e4f
vini <viniciuscipolli@gmail.com>
2021-07-25 23:00:53 -0300

    Paginação e inicio da config do módulo de Spring Security

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,18 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -39,17 +45,20 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDto> lista(String nomeCurso) {
-		List<Topico> topicos;
+	@Cacheable(value = "listaDeTopicos")
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
+		Page<Topico> topicos;
 		if (nomeCurso == null) {
-			topicos = topicoRepository.findAll();
+			topicos = topicoRepository.findAll(paginacao);
 		} else {
-			topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+			topicos = topicoRepository.findByCurso_Nome(nomeCurso, paginacao);
 		}
 		return TopicoDto.converter(topicos);
 	}
 
 	@PostMapping
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm,
 			UriComponentsBuilder uriComponentsBuilder) {
 		Topico topico = topicoForm.converter(cursoRepository);
@@ -71,6 +80,7 @@ public class TopicosController {
 	}
 
 	@PutMapping("/{id}")
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id,
 			@RequestBody @Valid AtualizacaoTopicoForm atualizacaoTopicoForm) {
 
@@ -79,12 +89,11 @@ public class TopicosController {
 			Topico topico = atualizacaoTopicoForm.atualizar(optTopico.get());
 			return ResponseEntity.ok(new TopicoDto(topico));
 		}
-
 		return ResponseEntity.notFound().build();
-
 	}
 
 	@DeleteMapping("/{id}")
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 
 		Optional<Topico> optTopico = topicoRepository.findById(id);
--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,9 +1,25 @@
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
+INSERT INTO CURSO(nome, categoria) VALUES('Spring microservices', 'Back-end');
+INSERT INTO CURSO(nome, categoria) VALUES('CSS', 'Front-end');
+INSERT INTO CURSO(nome, categoria) VALUES('Angular', 'Front-end');
+INSERT INTO CURSO(nome, categoria) VALUES('Node', 'Back-end');
 
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Zuleide', 'zuleide@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Afranio', 'afranio@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Estopa', 'estopa@email.com', '123456');
 
-INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
-INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 2', 'Projeto não compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
-INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida', 'Erro ao criar projeto', '2021-01-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 2', 'Projeto não compila', '2020-03-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 3', 'Tag HTML', '2019-08-05 20:00:00', 'NAO_RESPONDIDO', 2, 2);
+
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 4', 'Como colocar num docker', '2019-12-05 20:00:00', 'NAO_RESPONDIDO', 3, 3);
+
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 5', 'Two way databinding não funciona', '2020-01-05 20:00:00', 'NAO_RESPONDIDO', 2, 4);
+
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 6', 'Filtro quebrado', '2019-09-05 20:00:00', 'NAO_RESPONDIDO', 3, 5);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 6', 'Estilo zoua a pagina', '2021-05-05 20:00:00', 'NAO_RESPONDIDO', 3, 4);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 6', 'O que é isso?', '2021-07-05 20:00:00', 'NAO_RESPONDIDO', 3, 6);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 6', 'Node --v errado', '2019-04-05 20:00:00', 'NAO_RESPONDIDO', 3, 6);
