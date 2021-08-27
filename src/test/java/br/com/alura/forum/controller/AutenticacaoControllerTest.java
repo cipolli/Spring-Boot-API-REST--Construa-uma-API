33c70ffa950028f757ba3c89c208974dbb8b7b88
vini <viniciuscipolli@gmail.com>
2021-08-27 20:45:06 -0300

    Aula-04-atv-04
    https://cursos.alura.com.br/course/spring-boot-profiles-testes-deploy/task/81033

--- src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
+++ src/test/java/br/com/alura/forum/controller/AutenticacaoControllerTest.java
@@ -5,7 +5,6 @@ import java.net.URISyntaxException;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -14,7 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@DataJpaTest
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
